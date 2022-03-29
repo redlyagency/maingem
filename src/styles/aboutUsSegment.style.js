988cd0ae9802c732d64543100320b339b5e4e669
KrystianGH <krystus.serwis@gmail.com>
2022-03-29 18:44:56 +0200

    dodanie icon oraz praca nad rozmieszczeniem textu

--- src/styles/aboutUsSegment.style.js
+++ src/styles/aboutUsSegment.style.js
@@ -1,6 +1,9 @@
 import styled from "styled-components"
 
-import LoyaltyIcon from "../images/loyalty-icon.svg"   
+import LoyaltyIcon from "../images/loyalty-icon.svg"
+import PassionIcon from "../images/passion-icon.svg"
+import TransparencyIcon from "../images/transparency-icon.svg"
+import IndependenceIcon from "../images/independence-icon.svg"  
 
 export const AboutUsWrapper = styled.div`
 background: transparent linear-gradient(253deg, #FFFFFF 0%, #E2E2E2 100%) 0% 0% no-repeat padding-box;
@@ -16,14 +19,52 @@ export const IconLoyalty = styled.div`
     height: 50px;
     width: 50px;
 `
-
+export const IconPassion = styled.div`
+    background-image: url(${PassionIcon});
+    background-size: 100%;
+    height: 45px;
+    width: 50px;
+`
+export const IconTransparency = styled.div`
+    background-image: url(${TransparencyIcon});
+    background-size: 100%;
+    height: 50px;
+    width: 50px;
+`
+export const IconIndependence = styled.div`
+    background-image: url(${IndependenceIcon});
+    background-size: 100%;
+    height: 50px;
+    width: 50px;
+`
+export const HeaderDiv = styled.div`
+    height: 5vh;
+    width: 100vh;
+    padding-top: 350px;
+    padding: 60px 100px;
+`
 export const H1 = styled.h1`
     color: #38004F;
-    font-size: 45px;
-    left: 150px;
-    right: auto;
-    height: 61px;
     font: normal normal 600 45px/57px Nunito;
     opacity: 1;
-
 `
+export const DivIcon = styled.div`
+    height: 5vh;
+    width: 100vh;
+    background-position: center;
+    padding-top: 350px;
+    float: left;
+`
+export const IconName = styled.h3`
+    color: #38004F;
+    text-align: center;
+    font: normal normal 800 35px/44px Nunito;
+    opacity: 1;
+`
+export const TextContent = styled.h3`
+    color: #38004F;
+    text-align: center;
+    font: normal normal normal 30px/31px Nunito;
+    opacity: 1;
+`
+
--- src/templates/aboutUsSegment.js
+++ src/templates/aboutUsSegment.js
@@ -1,16 +1,45 @@
 import React from "react";
 import{
     AboutUsWrapper,
+    DivIcon,
     H1,
+    HeaderDiv,
+    IconContent,
+    IconIndependence,
     IconLoyalty,
+    IconPassion,
+    IconTransparency,
+    TextContent
 
 }from "../styles/aboutUsSegment.style"
+
+const TextList = [
+    {
+        LoyaltyContent: "Aliquam consequat eget justo vitae laoreet. Aliquam sapien nisi, convallis non lectus eget, condimentum tempus lacus. Nulla quis augue rhoncus magna vestibulum eleifend. Sed mollis viverra turpis ut accumsan. Fusce finibus bibendum interdum. Trices posuere cubilia curae; Etiam eu aliquam risus. Vivamus tempor dui posuere enim ultricies.",
+    },
+    {
+        PassionContent: "Sed lacus leo, pellentesque eget lacinia quis, cursus et orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vivamus sodales justo eu egestas ultrices. Fusce rhoncus placerat diam, quis rutrum tortor. Aliquam ac sem a enim condimentum sodales quis nec ligula. Praesent massa libero, aliquam vulputate libero ut.",
+    },
+    {
+        TransparencyContent: "In sed viverra urna. Vestibulum sit amet libero eget sapien posuere ornare. Integer euismod velit vel fringilla ultricies. Morbi est nibh, sodales ac vulputate et, posuere non lacus. Sed ac magna gravida, volutpat ante at, interdum erat. Etiam lobortis ac risus ut imperdiet. Vivamus et accumsan tellus, a euismod risus. Nulla sollicitudin pharetra sem, eu lobortis.",
+    },
+    {
+        IndependenceContent: "1Vestibulum sed tellus commodo, volutpat purus in, tincidunt nibh. Nullam dictum at quam sit amet facilisis. Nulla ante orci, viverra a placerat ut, efficitur at ligula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc in urna sit amet metus iaculis sagittis. Quisque laoreet faucibus libero a venenatis.",
+    }
+];
 
 const AboutUsSegment = () => {
     return(
         <AboutUsWrapper>
-            <H1>A few words about us</H1>
-            <IconLoyalty/>
+            <HeaderDiv>
+                <H1>A few words about us</H1>
+            </HeaderDiv>
+            <IconContent>
+                <IconLoyalty/>
+                <IconPassion/>
+                <IconTransparency/>
+                <IconIndependence/>
+            </IconContent>
         </AboutUsWrapper>
     )
 }
