96f39e2e0584dd920c05acad75a20412ff7e028b
KrystianGH <krystus.serwis@gmail.com>
2022-03-29 22:50:09 +0200

    small changes in mobile style

--- src/styles/aboutUsSegment.style.js
+++ src/styles/aboutUsSegment.style.js
@@ -3,10 +3,10 @@ import styled from "styled-components"
 export const AboutUsWrapper = styled.div`
     background: linear-gradient(253deg, #FFFFFF 0%, #E2E2E2 100%);
     opacity: 1;
-    padding: 100px 0;
+    padding: 100px 0px;
     height: 100vh;
     width: 100vw;
-    max-width: 1920px;
+    max-width: 100vw;
     display: flex;
     flex-direction: column;
 
@@ -23,6 +23,7 @@ export const BodyWrapper = styled.div`
 
     @media (max-width: 800px) {
         flex-direction: column;
+        padding: 30px 50px;
     }
 `
 export const ColumnWrapper = styled.div`
@@ -32,9 +33,17 @@ export const ColumnWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    @media (max-width: 800px) {
+        padding: 0 20px;
+    }
 `
 export const Title = styled.h6`
     padding-top: 10px;
+
+    @media (max-width: 800px) {
+        padding-top: 0px;
+    }
 `
 export const Content = styled.p`
     text-align: center;
@@ -47,12 +56,23 @@ export const Icon = styled.div`
     width: 60px;
     background-repeat: no-repeat;
     background-position: center;
+    flex-direction: column;
+
+    @media (max-width: 800px) {
+        height: 40px;
+        width: 40px;
+        align-items: center;
+    }
 `
 export const HeaderDiv = styled.div`
     height: 5vh;
     width: 100;
-    padding-top: 350px;
     padding: 60px 100px;
+
+    @media (max-width: 800px) {
+        height: auto;
+        padding: 60px 100px;
+    }
 `
 export const H1 = styled.h1`
     color: #38004F;
@@ -60,26 +80,6 @@ export const H1 = styled.h1`
     opacity: 1;
     
     @media (max-width: 800px) {
-        flex-direction: column;
+        font: normal normal 600 30px/43px Nunito;
     }
 `
-export const DivIcon = styled.div`
-    height: 5vh;
-    width: 100vh;
-    background-position: center;
-    padding-top: 350px;
-    float: left;
-`
-export const IconName = styled.h3`
-    color: #38004F;
-    text-align: center;
-    font: normal normal 800 35px/44px Nunito;
-    opacity: 1;
-`
-export const TextContent = styled.h3`
-    color: #38004F;
-    text-align: center;
-    font: normal normal normal 30px/31px Nunito;
-    opacity: 1;
-`
-
